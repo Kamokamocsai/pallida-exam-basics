3b402bdfe94fe48188e1869a78505b97703a2434
András Kamocsai <kamo.andras@gmail.com>
2017-10-06 12:00:55 +0200

    f

--- favouriteanimals/favourite_animals.py
+++ favouriteanimals/favourite_animals.py
@@ -4,16 +4,15 @@ class FavAnimals():
     def __init__(self):
         user_input = argv
         if len(user_input) == 1:
-            self.print_instructions()
+            print("fav_animals [animal] [animal]")
 
         elif len(user_input) == 2:
             with open('favourites.txt', 'a') as file:
-                file.write(str(user_input[1:]) + "\n")
+                file.write(str(user_input[1]) + "\n")
+            
+        elif len(user_input) == 3:
+            with open('favourites.txt', 'a') as file:
+                file.write(str(user_input[1]) + "\n" + str(user_input[2]) + "\n")
+                 
 
-    
-    def print_instructions(self):
-        print("fav_animals [animal] [animal]")
-
-
-controller = FavAnimals()
-    
+FavAnimals()    
